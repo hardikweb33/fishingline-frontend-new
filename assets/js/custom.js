a0857a66000c4dc4139135467f4c56f590ec8e53
hardikweb33 <hardikweb33@gmail.com>
2022-01-11 09:00:24 +0530

    home page

---
+++ assets/js/custom.js
@@ -0,0 +1,40 @@
+var $ = jQuery;
+$(window).on('load', function() { 
+ AOS.init({
+   once: true
+ });
+});
+
+$( document ).ready(function() {
+
+  AOS.init({
+    duration: 1200,
+  })
+
+  // hero slider
+  $('.hero-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true
+  });
+
+  $('.featur-slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true
+  });
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1){  
+    $('header').addClass("sticky");
+  }
+  else{
+    $('header').removeClass("sticky");
+  }
+});
